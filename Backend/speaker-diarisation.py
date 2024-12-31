2758ce474da9242c7729afb23e6f71fd3a54986b
taiye <taiyehige@gmail.com>
2024-12-31 10:30:28 +0000

    fix: secure key exposed

--- Backend/speaker-diarisation.py
+++ Backend/speaker-diarisation.py
@@ -27,10 +27,11 @@
 # for turn, _, speaker in diarization.itertracks(yield_label=True):
 #     print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
 
+import credentials
 from pyannote.audio import Pipeline
 pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization-3.1",
-    use_auth_token="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")
+    use_auth_token= credentials.HUGGING_FACE_KEY)#REPLACE
 
 # send pipeline to GPU (when available)
 import torch
